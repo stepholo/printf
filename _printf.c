8719c4d9a23fde0f6b44036389c66ac65f09cd11
stepholo <oloobrian89@gmail.com>
2023-03-26 09:58:23 -0700

    User Defined _printf

--- _printf.c
+++ _printf.c
@@ -1,47 +1,50 @@
 #include "main.h"
 
 /**
- * _printf - is a function that selects the correct function to print
- * @format: identifier to look for.
- * Return: the length of the string
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-
-int _printf(const char *format, ...)
+int _printf(char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+	int written = 0, (*structype)(char *, va_list);
+	char q[3];
+	va_list pa;
 
-	va_list args;
-	int i = 0, j, len = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	q[2] = '\0';
+	va_start(pa, format);
+	_putchar(-1);
+	while (format[0])
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[0] == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			structype = driver(format);
+			if (structype)
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				q[0] = '%';
+				q[1] = format[1];
+				written += structype(q, pa);
 			}
-			j--;
+			else if (format[1] != '\0')
+			{
+				written += _putchar('%');
+				written += _putchar(format[1]);
+			}
+			else
+			{
+				written += _putchar('%');
+				break;
+			}
+			format += 2;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			written += _putchar(format[0]);
+			format++;
+		}
 	}
-	va_end(args);
-	return (len);
+	_putchar(-2);
+	return (written);
 }
