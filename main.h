2025fc738d040cb0cf1a10fce1977de0e8bf3d96
stepholo <oloobrian89@gmail.com>
2023-03-26 09:58:22 -0700

    Header File

--- main.h
+++ main.h
@@ -1,49 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * main - Header file
- * Description: Header file containing prototypes
- */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include <unistd.h>
 
 /**
- * struct format - match the conversion specifiets for printf
- * @id: type char pointer of the specifier i.e (1, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
  *
+ * Return: int
  */
-
-typedef struct format
+typedef struct structprint
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
 
 int _putchar(char ch);
-int _printf(const char *format, ...);
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
 
 #endif
