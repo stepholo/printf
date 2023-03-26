a0581e5a25867c1619f38c847afa3f6747f2dba7
stepholo <oloobrian89@gmail.com>
2023-03-26 09:41:36 -0700

    Header File

--- main.h
+++ main.h
@@ -2,10 +2,48 @@
 #define MAIN_H
 
 /**
- * File: main.h
- * Decs: Header file containing prototypes
+ * main - Header file
+ * Description: Header file containing prototypes
  */
 
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct format - match the conversion specifiets for printf
+ * @id: type char pointer of the specifier i.e (1, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int _putchar(char ch);
 int _printf(const char *format, ...);
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+
 
 #endif
