b20ac53fae98132203ee3b0f0eafdc0db00f0833
stepholo <oloobrian89@gmail.com>
2023-03-27 03:06:35 -0700

    Team Work

--- _printf.c
+++ _printf.c
@@ -1,50 +1,39 @@
 #include "main.h"
-
 /**
- * _printf - Emulates printf() function in the standard library
- * @format: is the character string to be printed out
+ * _printf - function that emulates printf()
+ * @format: The character to be printed out
  * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
  */
-
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
 
-    int count = 0;
-    const char *p = format;
-    while (*p != '\0') {
-        if (*p == '%') {
-            switch (*(++p)) {
-                case 'c': {
-                    char c = (char) va_arg(args, int);
-                    _putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    const char *s = va_arg(args, const char *);
-                    while (*s != '\0') {
-                        _putchar(*s++);
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    _putchar('%');
-                    count++;
-                    break;
-                }
-            }
-        } else {
-            _putchar(*p);
-            count++;
-        }
-        p++;
-    }
+	va_start(args, format);
 
-    va_end(args);
-    return count;
+	int count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			switch (*++format)
+			{
+				case 'c':
+					count++, _putchar(va_arg(args, int));
+					break;
+				case 's':
+					for (const char *s = va_arg(args, const char *); *s; s++, count++)
+						_putchar(*s);
+					break;
+				case '%':
+					count++, _putchar('%');
+					break;
+			}
+		}
+		else
+			count++, _putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
-
