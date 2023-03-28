96a46d103b4e8429222c68c4ba5a8dff67c9a2d0
stepholo <oloobrian89@gmail.com>
2023-03-28 16:12:12 -0700

    Implementation of _printf()

---
+++ is_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
--- print_Hex_hex.c
+++ print_Hex_hex.c
@@ -1,44 +1,42 @@
 #include "main.h"
-
 /**
- * _printhex - prints a hexadecimal
+ * printhex - prints a hexadecimal
  * @format: format to print hexadecimal
  * @pa: list that contains the hexadecimal
- * Return: number
+ * Return: number.
  */
 int _printhex(char *format, va_list pa)
 {
 	unsigned int num = va_arg(pa, unsigned int);
 	unsigned int num2;
-	int i, i2, copy, contname = 0;
+	int i, i2, copy, contame = 0;
 	char *numhex;
 
 	(void)format;
 
 	if (num == 0)
 		return (_putchar('0'));
-	for (num2 = num; num2 != 0; contname++)
+	for (num2 = num; num2 != 0; contame++)
 	{
 		num2 = num2 / 16;
 	}
-	numhex = malloc(contname);
+	numhex = malloc(contame);
 	for (i = 0; num != 0; i++)
 	{
 		copy = num % 16;
 		if (copy < 10)
 			numhex[i] = copy + '0';
 		else
-			numhex[i] = copy - 10 + 'a';
+			numhex[i] = copy - 10  + 'a';
 		num = num / 16;
 	}
 	for (i2 = i - 1; i2 >= 0; i2--)
 		_putchar(numhex[i2]);
 	free(numhex);
-	return (contname);
+	return (contame);
 }
-
 /**
- * _printHEX - prints a hexadecimal
+ * printHEX - prints a hexadecimal
  * @format: format to print hexadecimal
  * @pa: va_list that contains the hexadecimal to print
  * Return: number of digits printed
@@ -47,18 +45,18 @@ int _printHEX(char *format, va_list pa)
 {
 	unsigned int NUM = va_arg(pa, unsigned int);
 	unsigned int NUM2;
-	int I, I2, COPY, CONTNAME = 0;
+	int I, I2, COPY, CONTAME = 0;
 	char *NUMHEX;
 
 	(void)format;
 
 	if (NUM == 0)
 		return (_putchar('0'));
-	for (NUM2 = NUM; NUM2 != 0; CONTNAME++)
+	for (NUM2 = NUM; NUM2 != 0; CONTAME++)
 	{
 		NUM2 = NUM2 / 16;
 	}
-	NUMHEX = malloc(CONTNAME);
+	NUMHEX = malloc(CONTAME);
 	for (I = 0; NUM != 0; I++)
 	{
 		COPY = NUM % 16;
@@ -71,5 +69,6 @@ int _printHEX(char *format, va_list pa)
 	for (I2 = I - 1; I2 >= 0; I2--)
 		_putchar(NUMHEX[I2]);
 	free(NUMHEX);
-	return (CONTNAME);
+	return (CONTAME);
 }
+
