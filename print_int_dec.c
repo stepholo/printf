b25683b72299c3d5d68acf556534517576122895
stepholo <oloobrian89@gmail.com>
2023-03-28 05:08:19 -0700

    Print %d and %i

---
+++ print_int_dec.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+/**
+ * printint - Prints an integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int _printint(va_list args)
+{
+	int n = va_args(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+/**
+ * printdec - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed.
+ */
+int _printdec(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = m;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
