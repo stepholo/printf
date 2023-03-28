7f938d09abda15c5852cbb85ba28ad8eaacd2aae
stepholo <oloobrian89@gmail.com>
2023-03-28 16:11:27 -0700

    Header File

--- main.h
+++ main.h
@@ -6,6 +6,20 @@
 #include <stdarg.h>
 #include <stddef.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
 /**
  * _printfstruct - stucture definition
  * @q:location and method to be translated
@@ -37,5 +51,13 @@ int print_hex_aux(unsigned long int num);
 int _printhex(char *format, va_list pa);
 int _printHEX(char *format, va_list pa);
 int _printocta(char *format, va_list pa);
+int print_rot13string(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_precision(const char *format, int *i, va_list list);
+int get_width(const char *format, int *i, va_list list);
+int is_digit(char c);
 
 #endif
